119ce270c76dde63679c212bed8fad37af6965ff
Victor Hugo <vhsouza606@gmail.com>
2024-01-21 20:00:24 -0300

    feat: adicionando header ao layout das páginas

---
+++ apps/web/app/(protected)/layout.tsx
@@ -0,0 +1,44 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Input } from '@/components/ui/input'
+
+import { Search } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-[100svh] w-full ">
+      <header className=" border-b px-4 py-2">
+        <div className="mx-auto flex max-w-7xl items-center justify-between gap-6">
+          <Image
+            src="/logo.png"
+            width={32}
+            height={32}
+            alt="VisualSocial logo"
+            quality={100}
+          />
+          <div className="relative w-full max-w-md">
+            <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input placeholder="Pesquisar" className="pl-8" />
+          </div>
+          <Link href="/account">
+            <div className="flex items-center gap-4">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>VS</AvatarFallback>
+              </Avatar>
+              <span className="hidden text-sm font-medium md:block">
+                usuario
+              </span>
+            </div>
+          </Link>
+        </div>
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
---
+++ apps/web/app/(protected)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-6">
+      <h1 className="text-xl font-semibold">Feed</h1>
+    </div>
+  )
+}
