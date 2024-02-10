d1c9ed759fffa853ecc78fc5dc13535781e5eb66
Luisfelipe114 <cabralluis994@gmail.com>
2024-02-10 20:15:51 -0300

    test: testando criacao de comentario

--- apps/server/src/user/post/post.service.spec.ts
+++ apps/server/src/user/post/post.service.spec.ts
@@ -1,9 +1,40 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep } from 'jest-mock-extended';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PostRepository } from '../repositories/post.repository';
 import { UserRepository } from '../repositories/user.repository';
+import { CreateCommentDto } from './dto/post.dto';
 import { PostService } from './post.service';
+
+const mockUserReturnData = {
+  id: 1,
+  name: 'john doe',
+  username: 'johnjohn',
+  email: 'john@gmail.com',
+  password: 'kskhskhdksjdow',
+  profilePicture: 'img1.png',
+  bio: 'hello world!',
+};
+
+const mockPostReturnData = {
+  id: 1,
+  userId: 1,
+  description: 'nice',
+  date: new Date(),
+};
+
+const mockCommentReturnData = {
+  id: 1,
+  postId: 1,
+  comment: 'Test Comment',
+  date: new Date(),
+  userId: 1,
+};
 
 describe('PostService', () => {
   let service: PostService;
@@ -16,16 +47,27 @@ describe('PostService', () => {
         PostService,
         PostRepository,
         UserRepository,
+        PrismaService,
         {
-          provide: PrismaService,
+          provide: UserRepository,
           useValue: {
-            postComments: {
-              findMany: jest.fn(),
-            },
+            findUserById: jest.fn().mockReturnValue(mockUserReturnData),
+          },
+        },
+        {
+          provide: PostRepository,
+          useValue: {
+            findPostById: jest.fn().mockReturnValue(mockPostReturnData),
+            findCommentByUserId: jest.fn().mockReturnValue(null),
+            getPostComments: jest.fn(),
+            createComment: jest.fn(),
           },
         },
       ],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
     service = module.get<PostService>(PostService);
     postRepository = module.get<PostRepository>(PostRepository);
@@ -73,6 +115,62 @@ describe('PostService', () => {
 
       await expect(service.getPostComments(1, 0, 10)).rejects.toThrow(
         'Erro interno ao buscar comentários do post',
+      );
+    });
+  });
+
+  describe('createComment', () => {
+    const mockCreateCommentData: CreateCommentDto = {
+      comment: 'Test Comment',
+    };
+
+    it('should creat a comment', async () => {
+      jest
+        .spyOn(postRepository, 'createComment')
+        .mockResolvedValue(mockCommentReturnData);
+
+      const result = await service.createComment(1, 1, mockCreateCommentData);
+
+      expect(result).toEqual({ id: mockCommentReturnData.id });
+    });
+
+    it('should throw an exception if user has already commented', async () => {
+      jest
+        .spyOn(postRepository, 'createComment')
+        .mockRejectedValue(
+          new ConflictException(
+            'Não é possível fazer mais de um comentário por postagem',
+          ),
+        );
+
+      jest
+        .spyOn(postRepository, 'findCommentByUserId')
+        .mockResolvedValue(mockCommentReturnData);
+
+      await expect(
+        service.createComment(1, 1, mockCreateCommentData),
+      ).rejects.toThrow(
+        new ConflictException(
+          'Não é possível fazer mais de um comentário por postagem',
+        ),
+      );
+    });
+
+    it('should throw an exception if an error occurs', async () => {
+      jest
+        .spyOn(postRepository, 'createComment')
+        .mockRejectedValue(
+          new InternalServerErrorException(
+            'Erro interno ao criar novo comentário',
+          ),
+        );
+
+      await expect(
+        service.createComment(1, 1, mockCreateCommentData),
+      ).rejects.toThrow(
+        new InternalServerErrorException(
+          'Erro interno ao criar novo comentário',
+        ),
       );
     });
   });
