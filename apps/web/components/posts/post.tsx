dd20b801a84f16d5054b2e083e7fedd3fff252f5
lhaquino22 <99754185+lhaquino22@users.noreply.github.com>
2024-02-17 16:03:31 -0300

    Alterando Post, adicionando icones de Like, Dislike e Comentários

--- apps/web/components/posts/post.tsx
+++ apps/web/components/posts/post.tsx
@@ -1,10 +1,27 @@
-import { Post } from '@/types/post'
+import { Post as PostType } from '@/types/post'
+import { ThumbsUp, ThumbsDown, MessageSquare } from 'lucide-react'
+import { Button } from '../ui/button'
 
-export function Post(post: Post) {
+export function Post(post: PostType) {
   return (
     <div key={post.id} className="rounded-md border p-20 shadow">
       <h2 className="text-2xl font-semibold">Post {post.id}</h2>
       <p className="text-muted-foreground">{post.description}</p>
+
+      {/* Botões para Like, Dislike e Comentário */}
+      <div className="mt-4 flex">
+        <Button size="icon">
+          <ThumbsUp className="h-6 w-6 text-blue-500" />{' '}
+        </Button>
+
+        <Button size="icon" className="ml-4">
+          <ThumbsDown className="h-6 w-6 text-red-500" />{' '}
+        </Button>
+
+        <Button size="icon" className="ml-4">
+          <MessageSquare className="h-6 w-6 text-green-500" />
+        </Button>
+      </div>
     </div>
   )
 }
