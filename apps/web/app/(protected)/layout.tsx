cb9d180443a9388a78141934af657dbeba8ec82e
Victor Hugo <vhsouza606@gmail.com>
2024-02-18 18:50:52 -0300

    feat: adicionando link para feed na logo do visual social; aumento do tamanho da logo e avatar

--- apps/web/app/(protected)/layout.tsx
+++ apps/web/app/(protected)/layout.tsx
@@ -16,16 +16,18 @@ export default async function ProtectedLayout({
   } = await getServerSession()
   return (
     <div className="min-h-[100svh] w-full ">
-      <header className=" border-b px-4 py-2">
+      <header className=" border-b px-4 py-3">
         <div className="mx-auto flex max-w-7xl items-center justify-between gap-6">
-          <Image src={Logo} alt="VisualSocial logo" className="size-8" />
+          <Link href="/">
+            <Image src={Logo} alt="VisualSocial logo" className="size-10" />
+          </Link>
           <div className="relative w-full max-w-md">
             <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input placeholder="Pesquisar" className="pl-8" />
           </div>
           <Link href="/account">
             <div className="flex items-center gap-4">
-              <Avatar>
+              <Avatar className="size-10">
                 {user?.profilePictureUrl && (
                   <AvatarImage src={user?.profilePictureUrl} />
                 )}
